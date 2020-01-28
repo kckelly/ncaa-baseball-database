ffe7e52cd7dd527cbf6a0a55f252cf7cac1f4049
Kevin <kevinclaytonkelly@gmail.com>
2020-01-27 22:17:03 -0500

    Streamline WebUtils and Add Helper Methods
    WebUtils' most important method, get_page, was
    changed to use requests instead of urllib.request,
    since the program was sometimes hanging up while
    accessing a page. The requests library also seems
    faster.
    
    Multiple helper methods were also added to WebUtils
    for getting ids from urls.

--- scrapers/WebUtils.py
+++ scrapers/WebUtils.py
@@ -5,8 +5,7 @@ import sys
 
 from bs4 import BeautifulSoup
 import time
-from urllib.error import HTTPError
-from urllib.request import Request, urlopen
+import requests
 
 
 def get_page(url, sleep_time, try_limit):
@@ -20,13 +19,61 @@ def get_page(url, sleep_time, try_limit):
     @return: the page in a BeautifulSoup object with the parser set to 'html.parser'
     """
     for tries in range(0, try_limit):
-        time.sleep(sleep_time)
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        headers = {'User-Agent': 'Mozilla/5.0', 'Connection': 'close'}
         try:
-            page = urlopen(req)
-            return BeautifulSoup(page, 'html.parser')
-        except HTTPError:
+            t0 = time.time()
+            page = requests.get(url, headers=headers)
+            print('{:.2f} seconds... '.format(time.time() - t0), end='')
+            return BeautifulSoup(page.text, 'html.parser')
+        except requests.exceptions.RequestException as e:
             print('\nFailed to connect to {url} trying again. Try {try_number}.'.
                   format(url=url, try_number=tries))
-    print('\tUnable to connect to {url}'.format(url=url))
+            if tries == try_limit - 1:
+                print('Unable to connect to {url}'.format(url=url))
+                print('\t' + str(e))
+        time.sleep(sleep_time)
     sys.exit(1)
+
+
+def get_school_id_from_url(url):
+    """
+    Get the school id from the url of a ncaa team.
+    
+    @param url: the team url
+    @return: the school id of the team
+    """
+    return url.split('/')[2]
+
+
+def get_coach_id_from_url(url):
+    """
+    Get the coach's id from their url.
+
+    @param url: the coach's url
+    @return: the coach's id
+    """
+    return url.split('/')[2].split('?')[0]
+
+
+def get_player_id_from_url(url):
+    """
+    Get the player's id from their url.
+
+    @param url: the player's url
+    @return: the player's id
+    """
+    return url[url.rindex('=') + 1:]
+
+
+def get_contest_id_from_url(url, year):
+    """
+    Get the game's id from its url.
+
+    @param url: the game's url
+    @param year: the year of the game the url came from
+    @return: the game's id
+    """
+    if year < 2019:
+        return url.split('?')[0].split('/')[3][:]
+    else:
+        return url.split('/')[2]
